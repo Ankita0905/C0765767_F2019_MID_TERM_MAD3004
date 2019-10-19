a6fa254dba09f214cb7855fa3a037358ff49142c
Ankita Jain <ankita.jain12342@gmail.com>
2019-10-19 18:13:02 -0400

    created extension for GB used

---
+++ C0765767_F2019_MID_TERM_MAD3004/Extensions/DataUsed.swift
@@ -0,0 +1,17 @@
+//
+//  DataUsed.swift
+//  C0765767_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension Float
+{
+    func gbUsed() -> String {
+        return "\(self) GB"
+    }
+    
+    
+}
