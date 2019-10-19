f6b724c39e5a0dadadf9709e6b4d3a21f0bb5f90
Ankita Jain <ankita.jain12342@gmail.com>
2019-10-19 18:58:43 -0400

    updated main

--- C0765767_F2019_MID_TERM_MAD3004/main.swift
+++ C0765767_F2019_MID_TERM_MAD3004/main.swift
@@ -8,22 +8,26 @@
 
 import Foundation
 
-var i = Internet(providerName: "Rogers", gbUsed: 55.8, billId: "IN01", billDate: "", billAmount: 100, billType: Bill.Types.Internet)
-var m = Mobile(manufacturerName: "Apple", planName: "AA", mobileNumber: "987678965", internetUsed: 7.9, minutesUsed: 500, billAmount: 120, billId: "M01", billType:Bill.Types.Mobile, billDate: "")
-var h = Hydro(agencyName: "Brampton Energy", unitsConsumed: 200, billAmount: 150, billId: "H01", billType: Bill.Types.Hydro, billDate: "")
+let formatter = DateFormatter()
+formatter.dateFormat = "yyyy/MM/dd"
+let d1 = formatter.date(from: "2019/03/23")?.dateformatter()
+let d2 = formatter.date(from: "2018/12/12")?.dateformatter()
+let d3 = formatter.date(from: "2019/10/02")?.dateformatter()
 
-var c = Customer(customerId: "101", firstName: "Ankita ", lastName: "Jain", email: "ankita@gmail.com", billDictionary : [i.billId:i,m.billId:m,h.billId:h])
 
+
+var i = Internet(providerName: "Rogers", gbUsed: 8, billId: "IN01", billDate: d1!, billAmount: 50, billType: Bill.Types.Internet)
+var m = Mobile(manufacturerName: "Apple", planName: "AA", mobileNumber: "545643654", internetUsed: 505, minutesUsed: 500, billAmount: 70, billId: "M01", billType:Bill.Types.Mobile, billDate: d3!)
+var h = Hydro(agencyName: "Brampton Energy", unitsConsumed: 51015, billAmount: 150, billId: "H01", billType: Bill.Types.Hydro, billDate: d2!)
+
+var c = Customer(customerId: "101", firstName: "Ankita", lastName: "Jain", email: "ankita@gmail.com", billDictionary : [i.billId:i,m.billId:m,h.billId:h])
 c.iDisplay()
 
-var i1 = Internet(providerName: "Rogers", gbUsed: 100.9, billId: "I001", billDate: "", billAmount: 89.5, billType: Bill.Types.Internet)
-var h1 = Hydro(agencyName: "Planet Energy", unitsConsumed: 67.8, billAmount: 67, billId: "H001", billType: Bill.Types.Hydro, billDate: "")
-var c1 = Customer(customerId: "102", firstName: "Kamal ", lastName: "Kaur", email: "kamal@gmail.com", billDictionary : [i1.billId:i1,h1.billId:h1])
+var i1 = Internet(providerName: "Freedom", gbUsed: 7.5, billId: "IN02", billDate: d1!, billAmount: 90, billType: Bill.Types.Internet)
+var h1 = Hydro(agencyName: "Tronto Energy", unitsConsumed: 78.31, billAmount: 100, billId: "H21", billType: Bill.Types.Hydro, billDate: d3!)
+var c1 = Customer(customerId: "102", firstName: "Kamal", lastName: "Kaur", email: "kamal@gmail.com", billDictionary : [i1.billId:i1,h1.billId:h1])
 
 c1.iDisplay()
-
-
-
 
 
 
