eceab07a6ca89cd63bf10ed7bfe0eb9ae77757cd
Ankita Jain <ankita.jain12342@gmail.com>
2019-10-19 19:07:26 -0400

    formatting output

--- C0765767_F2019_MID_TERM_MAD3004/Mobile.swift
+++ C0765767_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -27,6 +27,7 @@ class Mobile : Bill{
     }
     
     override func iDisplay() {
+        print("------------Bill Details-----------")
         print("Bill Id : \(billId)")
         print("Bill Type : \(billType)")
         print("Bill Date : \(billDate)")
